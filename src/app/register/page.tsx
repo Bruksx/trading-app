3e889b776d5ee71a1be9933e1d9c69bd8e204a84
muhammvd <muhammadsaniharuna44@gmail.com>
2023-06-12 07:58:17 +0100

    sign up page done

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
       </div>
       <div className="md:w-1/3 flex items-center justify-center px-16 py-12 bg-black">
         <form className="w-full">
-          <h1 className="mb-4 text-2xl font-bold text-white px-5">Log into your account</h1>
+          <h1 className="mb-4 text-2xl font-bold text-white px-5">Register a new account</h1>
           <div className="mb-4">
             <label htmlFor="email" className="block mb-3 font-semibold">
               Email
@@ -29,36 +29,28 @@ const LoginPage = () => {
             <input
               type="password"
               id="password"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-900 font-bold text-white"
+              className="w-full px-3 py-2 border border-gray-300 mb-3 rounded-md bg-gray-900 font-bold text-white"
               placeholder='Password'
             />
           </div>
-          <div className="flex items-center justify-between mb-3 mt-5">
-          <div className="flex items-center mb-6">
+          <div className="mb-4">
             <input
-              type="checkbox"
-              id="rememberPassword"
-              className="mr-2"
+              type="password"
+              id="password"
+              className="w-full px-3 py-2 border mb-3 border-gray-300 rounded-md bg-gray-900 font-bold text-white"
+              placeholder='Confirm Password'
             />
-            <label htmlFor="rememberPassword" className='text-1xl text-white'>Remember Password</label>
-          </div>
-         <a
-              href="#"
-              className="text-gray-600 hover:text-gray-800 text-sm mb-7"
-            >
-              Forgot Password?
-            </a>
           </div>
           
           <div className="flex items-center justify-between">
             <button
               type="submit"
-              className="w-full bg-white hover:bg-blue-600 hover:text-whi text-gray font-semibold py-2 px-4 rounded-md"
+              className="w-full bg-white hover:bg-blue-600 text-gray font-semibold py-2 px-4 rounded-md"
             >
-              Sign In
+              Sign Up
             </button>    
           </div>
-          <h2 className='text-white px-4 color mt-10 text-sm'>Don't have an account? <span className='font-bold'> <a href="/register"> Register here</a></span></h2>
+          <h2 className='text-white px-4 color mt-10 text-sm'>Already have an account? <span className='font-bold'> <a href="/login">Login here</a></span></h2>
         </form>
       </div>
     </div>
