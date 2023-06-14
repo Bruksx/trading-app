f1bf1265d35e3284859e69e1d69d3d06ec599171
muhammvd <muhammadsaniharuna44@gmail.com>
2023-06-14 18:55:50 +0100

    homepage++

---
+++ src/app/Carousel.js
@@ -0,0 +1,66 @@
+"use client"
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import "./styles.css";
+
+// import required modules
+import { Navigation } from "swiper";
+
+export default function App() {
+  return (
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>
+            <section className="bg-stone-700">
+            <div className="contner">
+            <img src="/in-liquid-slide-bg.png" alt="Wave Background" className="spread" />
+            <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center justify-center py-8 lg:py-16">
+            <div className="lg:w-1/2">
+            <div className="max-w-lg mx-auto text-center lg:text-left">
+               <h1 className="text-4xl lg:text-6xl font-bold mb-4 text-white">New standard in stock broker.</h1>
+                <p className="text-lg mb-8 text-white">Trade forex, commodities, synthetic and stock indices from a single account</p>
+                <button className="hover:border-white bg-transparent hover:bg-blue text-white font-bold border-solid border-2 border-red-400 py-2 px-4 rounded">
+                Discover Platform
+                </button>
+            </div>
+            </div>
+            <div className="lg:w-1/2">
+            <img src="/in-liquid-slide-1.svg" alt="Wave Background" className="w-2/2 h-45" />
+            </div>
+         
+        </div>
+        </div>
+        </section>
+        </SwiperSlide>
+        <SwiperSlide>
+        <section className="bg-stone-700">
+            <div className="contner">
+            <img src="/in-liquid-slide-bg.png" alt="Wave Background" className="spread" />
+            <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center justify-center py-8 lg:py-16">
+            <div className="lg:w-1/2">
+            <div className="max-w-lg mx-auto text-center lg:text-left">
+               <h1 className="text-4xl lg:text-6xl font-bold mb-4 text-white">Multi-regulated global broker.</h1>
+                <p className="text-lg mb-8 text-white">A trusted destination for traders worldwide, Authorised by FCA, ASIC & FSCA</p>
+                <button className="hover:border-white bg-transparent hover:bg-blue text-white font-bold border-solid border-2 border-red-400 py-2 px-4 rounded">
+                Discover Platform
+                </button>
+            </div>
+            </div>
+            <div className="lg:w-1/2">
+            <img src="/in-liquid-slide-2.svg" alt="Wave Background" className="w-2/2 h-45" />
+            </div>
+         
+        </div>
+        </div>
+        </section>  
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,126 @@
 import Image from 'next/image';
-import {Carousel} from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-
+import Carousel from './Carousel';
+import "./styles.css";
 
 export default function Home() {
   return (
     <main className="min-h-screen justify-between">
-       <header className="bg-stone-700 text-white">
-          <div className="container mx-auto flex items-center justify-between py-4 px-8">
-            <div className="flex items-center">
-              <img src="/logo.png" alt="Logo" className="h-8 mr-4" />
-            </div>
-            <nav className="space-x-4">
-              <a href="#" className="hover:text-neutral-400">Home</a>
-              <a href="#" className="hover:text-neutral-400">Markets</a>
-              <a href="#" className="hover:text-neutral-400">Company</a>
-              <a href="#" className="hover:text-neutral-400">Education</a>
-              <a href="#" className="hover:text-neutral-400">Resources</a>
-            </nav>
-            <button className="bg-red-600 hover:bg-red-400 hover:text-white font-bold text-stone-700 text-sm py-2 px-4 rounded">
-             <a href="/register">CREATE ACCOUNT</a> 
-            </button>
-          </div>
-        </header>
-        <section className="bg-stone-700 text-white">
-      <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center justify-center py-8 lg:py-16">
-        <div className="lg:w-1/2">
-          <img src="/in-liquid-slide-1.svg" alt="Wave Background" className="w-2/2 h-50" />
-        </div>
-        <div className="lg:w-1/2">
-          <div className="max-w-lg mx-auto text-center lg:text-left">
-            <h1 className="text-4xl lg:text-6xl font-bold mb-4">New standard in stock broker.</h1>
-            <p className="text-lg mb-8">Trade forex, commodities, synthetic and stock indices from a single account</p>
-            <button className="hover:border-white bg-transparent hover:bg-blue text-white font-bold border-solid border-2 border-red-400 py-2 px-4 rounded">
-              Discover Platform
-            </button>
-          </div>
+      <header className="bg-stone-700 text-white sticky top-0 z-50 ">
+  <div className="container mx-auto flex items-center justify-between py-4 px-8">
+    <div className="flex items-center">
+      <img src="/logo.png" alt="Logo" className="h-12 mr-4" />
+    </div>
+    <nav className="space-x-4">
+      <a href="#" className="hover:text-neutral-400">Home</a>
+      <a href="#" className="hover:text-neutral-400">Markets</a>
+      <div className="dropdown inline-block relative">
+        <button className="hover:text-neutral-400">Company</button>
+        <ul className="dropdown-content absolute hidden bg-white text-stone-700 pt-2">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Careers</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+      </div>
+      <a href="#" className="hover:text-neutral-400">Education</a>
+      <a href="#" className="hover:text-neutral-400">Resources</a>
+    </nav>
+    <button className="bg-red-600 hover:bg-red-400 hover:text-white font-bold text-stone-700 text-sm py-2 px-4 rounded">
+      <a href="/register">CREATE ACCOUNT</a>
+    </button>
+  </div>
+</header>
+      <Carousel/>
+      <div className="second-showcase flex justify-between bg-black px-20px">
+      <div>
+        <p className="text-4xl font-bold text-white">
+          Save time. Get <span className='text-red-400'>higher return</span>. <br /> Multiply wealth.
+        </p>
+      </div>
+      <div>
+        <button className="bg-transparent-500 hover:bg-red-500 text-white font-bold py-3 px-8 border border-red-500 border-solid rounded">
+          Find out more
+        </button>
+      </div>
+    </div>
+
+    <div className="sec-showcase grid grid-cols-3 gap-9 bg-black">
+      <div className="grd bg-white p-4 shadow-md">
+        <img
+          src="/in-liquid-object-1.svg"
+          alt="Card 1"
+          className="w-full h-40 object-cover mb-2"
+        />
+        <p className="text-lg font-semibold text-white py-2">Various assets</p>
+        <p className="text-gray-600 text-white py-6">Duis aute irure dolor in reprehen derit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+      <div className="grd bg-white p-4 shadow-md">
+        <img
+          src="/in-liquid-object-2.svg"
+          alt="Card 2"
+          className="w-full h-40 object-cover mb-2"
+        />
+        <p className="text-lg font-semibold text-white py-2">Market analysis</p>
+        <p className="text-gray-600 text-white py-6">Duis aute irure dolor in reprehen derit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+      <div className="grd bg-white p-4 shadow-md">
+        <img
+          src="/in-liquid-object-2.svg"
+          alt="Card 3"
+          className="w-full h-40 object-cover mb-2"
+        />
+        <p className="text-lg font-semibold text-white py-2">Enhanced tools</p>
+        <p className="text-gray-600 text-white py-6">Duis aute irure dolor in reprehen derit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+
+    <div className="third-container">
+
+      <div className="first-flex">
+      <div className="first-flex-container">
+        <img src="/in-liquid-icon-1.svg" alt="icon"  />
+        <div className="text-blurb">
+          <h1>Expert Service</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium veritatis asperiores placeat quisquam voluptas repellendus.</p>
+          <button>LEARN MORE</button>
         </div>
       </div>
-    </section>
+
+      <div className="first-flex-container">
+        <img src="/in-liquid-icon-2.svg" alt="icon"  />
+        <div className="text-blurb">
+          <h1>Fully regulated</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium veritatis asperiores placeat quisquam voluptas repellendus.</p>
+          <button>LEARN MORE</button>
+        </div>
+      </div>
+      
+      </div>
+
+      <div className="first-flex py-10">
+      <div className="first-flex-container">
+        <img src="/in-liquid-icon-3.svg" alt="icon"  />
+        <div className="text-blurb">
+          <h1>Financial Strength</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium veritatis asperiores placeat quisquam voluptas repellendus.</p>
+          <button>LEARN MORE</button>
+        </div>
+      </div>
+
+      <div className="first-flex-container">
+        <img src="/in-liquid-icon-4.svg" alt="icon"  />
+        <div className="text-blurb">
+          <h1>Integrated Support</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium veritatis asperiores placeat quisquam voluptas repellendus.</p>
+          <button>LEARN MORE</button>
+        </div>
+      </div>
+      
+      </div>
+
+
+      
+    </div>
     </main>
   )
 }
