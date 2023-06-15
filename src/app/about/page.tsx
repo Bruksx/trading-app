fc5b2899de374bea0a4c376fa21c16e39a611011
muhammvd <muhammadsaniharuna44@gmail.com>
2023-06-15 23:19:27 +0100

    about page++

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,20 @@
+"use client"
 import Image from 'next/image';
 import "../styles.css";
+import React, { useEffect, useState } from 'react';
 
 export default function Market() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (count < 213) {
+        setCount(count + 1);
+      }
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, [count]);
   return (
     <main className="min-h-screen justify-between">
       <header className="bg-stone-700 text-white sticky top-0 z-50 ">
@@ -54,19 +67,20 @@ export default function Market() {
 
 
     <div className="bgg  w-3/4 h-full mx-auto bg-no-repeat bg-cover py-6 px-4 md:px-10 lg:px-20">
-      <div className="max-w-3xl mx-auto">
-        <h1 className="text-4xl font-bold text-center mb-6">Trust the Professionals</h1>
-        <p className="text-lg text-center mb-8 mt-6">We are a group of passionate, independent thinkers who never stop exploring new ways to improve trading for the self-directed invest</p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-          <div className="flex items-center">
+      <div className="max-w-6xl mx-auto">
+        <h1 className="text-4xl font-bold text-center mb-6 mt-4">Trust the Professionals</h1>
+        <p className="text-lg text-center mb-10 mt-6 text-gray-400">We are a group of passionate, independent thinkers who never stop exploring new ways to improve trading for the self-directed invest</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-16 mt-4">
+
+        <div className="flex items-start mb-10">
             <div className="mr-4">
-              <Image src="/image1.jpg" alt="Image 1" width={100} height={100} />
+              <Image src="/in-team-1.png" alt="Image 1" width={450} height={450} />
             </div>
             <div>
-              <p>Paragraph text 1</p>
-              <p>Johnathan Taylor</p>
-              <p>Small description</p>
-              <div className="flex">
+              <p className='text-gray-400'>Cheif Executive Officer</p>
+              <p className='mb-4 mt-2 font-bold text-2xl'>Cynthia Taylor</p>
+              <p>Nostrum exercitationem ullam corporis suscipit labor samnisi ute aliquid fugiat.</p>
+              <div className="flex mt-5">
                 <a href="#" className="mr-2">
                   <Image src="/facebook-icon.png" alt="Facebook" width={20} height={20} />
                 </a>
@@ -80,15 +94,60 @@ export default function Market() {
             </div>
           </div>
 
-          <div className="flex items-center">
+          <div className="flex items-start mb-10">
             <div className="mr-4">
-              <Image src="/image2.jpg" alt="Image 2" width={100} height={100} />
+              <Image src="/in-team-2.png" alt="Image 2" width={450} height={450} />
             </div>
             <div>
-              <p>Paragraph text 2</p>
-              <p>Johnathan Taylor</p>
-              <p>Small description</p>
-              <div className="flex">
+              <p className='text-gray-400'>Executive assistant</p>
+              <p className='mb-4 mt-2 font-bold text-2xl'>Arthur Parker</p>
+              <p>Nostrum exercitationem ullam corporis suscipit labor samnisi ute aliquid fugiat.</p>
+              <div className="flex mt-5">
+                <a href="#" className="mr-2">
+                  <Image src="/facebook-icon.png" alt="Facebook" width={20} height={20} />
+                </a>
+                <a href="#" className="mr-2">
+                  <Image src="/twitter-icon.png" alt="Twitter" width={20} height={20} />
+                </a>
+                <a href="#">
+                  <Image src="/instagram-icon.png" alt="Instagram" width={20} height={20} />
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="flex items-start">
+            <div className="mr-4">
+              <Image src="/in-team-3.png" alt="Image 1" width={450} height={450} />
+            </div>
+            <div>
+              <p className='text-gray-400'>Cheif Executive Officer</p>
+              <p className='mb-4 mt-2 font-bold text-2xl'>Evelyn Mason</p>
+              <p>Nostrum exercitationem ullam corporis suscipit labor samnisi ute aliquid fugiat.</p>
+              <div className="flex mt-5">
+                <a href="#" className="mr-2">
+                  <Image src="/facebook-icon.png" alt="Facebook" width={20} height={20} />
+                </a>
+                <a href="#" className="mr-2">
+                  <Image src="/twitter-icon.png" alt="Twitter" width={20} height={20} />
+                </a>
+                <a href="#">
+                  <Image src="/instagram-icon.png" alt="Instagram" width={20} height={20} />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-start">
+            <div className="mr-4">
+              <Image src="/in-team-4.png" alt="Image 2" width={450} height={450} />
+            </div>
+            <div>
+              <p className='text-gray-400'>Executive assistant</p>
+              <p className='mb-4 mt-2 font-bold text-2xl'>Bryan Greene</p>
+              <p>Nostrum exercitationem ullam corporis suscipit labor samnisi ute aliquid fugiat.</p>
+              <div className="flex mt-5">
                 <a href="#" className="mr-2">
                   <Image src="/facebook-icon.png" alt="Facebook" width={20} height={20} />
                 </a>
@@ -102,6 +161,27 @@ export default function Market() {
             </div>
           </div>
         </div>
+      </div>
+    </div>
+
+    <div className="lst flex justify-evenly items-center mt-30">
+      <div className="max-w-6xl mr-50 rounded-lg p-8">
+        <div className=" mb-4">
+          <h2 className="text-2xl font-bold text-gray-500 py-3">Number speaks</h2>
+         <h1 className='text-6xl font-bold py-6'>We always ready <br />
+for a <span className='text-red-500'>challenge.</span> </h1>
+        </div>
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          Learn more
+        </button>
+      </div>
+
+      <div className="max-w-4xl bg-white rounded-lg p-8 ml-4">
+        <div className="flex justify-between items-start pb-2 mb-4 mr-40">
+          <span className="font-bold  text-6xl text-red-500 py-2 border-gray-700">{count}</span>
+          <span className="font-bold ml-4 text-2xl mt-2">Trading instruments</span>
+        </div>
+        <div className='text-gray-400'>Reprehenderit qui in svoluptate velit esse quam molestiae vel illum qui dolorem fugiat quo.</div>
       </div>
     </div>
 
