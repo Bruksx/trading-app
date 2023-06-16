df508621aea6bb16c6f3eebb5bcd101ebbd658dd
muhammvd <muhammadsaniharuna44@gmail.com>
2023-06-16 13:11:52 +0100

    about page done++

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,6 +5,18 @@ import React, { useEffect, useState } from 'react';
 
 export default function Market() {
   const [count, setCount] = useState(0);
+  const [secondCount, setsecondCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (secondCount < 27) {
+        setsecondCount(secondCount + 1);
+      }
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, [secondCount]);
+
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -176,13 +188,22 @@ for a <span className='text-red-500'>challenge.</span> </h1>
         </button>
       </div>
 
-      <div className="max-w-4xl bg-white rounded-lg p-8 ml-4">
-        <div className="flex justify-between items-start pb-2 mb-4 mr-40">
+      <div className="max-w-lg bg-white rounded-lg p-8 ml-4">
+        <div className="flex justify-left items-start pb-2 mb-8">
           <span className="font-bold  text-6xl text-red-500 py-2 border-gray-700">{count}</span>
-          <span className="font-bold ml-4 text-2xl mt-2">Trading instruments</span>
+          <div>
+          <span className="font-bold text-2xl ml-5 mt-2">Trading instruments</span>
+          <div className='text-gray-400 w-3/4 ml-5 mt-5'>Reprehenderit qui in svoluptate velit esse quam molestiae vel illum qui dolorem fugiat quo.</div>
+           </div> 
         </div>
-        <div className='text-gray-400'>Reprehenderit qui in svoluptate velit esse quam molestiae vel illum qui dolorem fugiat quo.</div>
-      </div>
+        <div className="flex justify-left items-start pb-2 mb-4 mt-4">
+          <span className="font-bold  text-6xl text-red-500 py-2 border-gray-700">{secondCount}</span>
+          <div>
+          <span className="font-bold text-2xl ml-5 mt-2">Countries covered</span>
+          <div className='text-gray-400 w-3/4 ml-5 mt-5'>Reprehenderit qui in svoluptate velit esse quam molestiae vel illum qui dolorem fugiat quo.</div>
+           </div> 
+        </div>
+        </div>
     </div>
 
 
